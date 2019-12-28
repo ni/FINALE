5174b86e2fc11a9ffd7d4f629e2cc05945351bbb
Pratheeksha S N <pratheeksha.s.n@ni.com>
2019-12-28 12:48:48 +0530

    wrap line

--- src/script/design/Node.ts
+++ src/script/design/Node.ts
@@ -7,7 +7,11 @@ export class NodeView extends ComponentView {
         // ElementView.addEvent(componentElement, "dblclick", this.openVI, obj.path);
         const anchor = ElementView.createElement(obj.bounds, obj.id + " " + obj.type, "a");
         const componentElement = super.generate(obj);
-        anchor.setAttribute("href", "http://www.ni.com/documentation/en/labview-comms/4.0/node-ref/" + (obj.path).toLowerCase());
+        const helpUrl = "http://www.ni.com/documentation/en/labview-comms/4.0/node-ref/" + (obj.path).toLowerCase();
+        anchor.setAttribute(
+            "href",
+            helpUrl,
+        );
         anchor.appendChild(componentElement);
         return anchor;
     }
