d73ab4b8d23fecc69054196a7de44f44e775a12e
Ayush Kumar Baheti <ayush.baheti.kumar@ni.com>
2020-05-07 18:31:25 +0530

    Session storage which stores DOMs of the files

--- src/script/finale.ts
+++ src/script/finale.ts
@@ -4,9 +4,15 @@ import { HelpProvider } from "./help/helpProvider";
 import { Model } from "./modelParser";
 import { PathHelper } from "./pathHelper";
 import { ViewFactory } from "./viewFactory";
+import { isNullOrUndefined } from "util";
 
 document.addEventListener("DOMContentLoaded", () => {
-    generateDocument(document.location.search);
+    if (isNullOrUndefined(sessionStorage.getItem(document.location.search.slice(4)))) {
+        generateDocument(document.location.search);
+        } else {
+            parsedContent = JSON.parse(sessionStorage.getItem(document.location.search.slice(4)));
+            LoadDOM(document.location.search);
+        } 
     document.onkeyup = (e) => {
         if (InputHelper.IsHelpKeyBinding(e)) {
             HelpProvider.toggleVisibility();
@@ -14,7 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 }, false);
 
+let parsedContent = null;
 let originPath = "";
+
+function LoadDOM(query : string) {
+    const viewObj = new ViewFactory();
+    const htmlDOM = viewObj.generate(parsedContent);
+    if (htmlDOM) {
+        const url = document.location.search + document.location.hash;
+        HistoryManager.UpdateHistory(url, parsedContent);
+        const finale = document.getElementById("divLog");
+        while (finale.hasChildNodes()) {
+            finale.removeChild(finale.lastChild);
+        }
+    finale.appendChild(htmlDOM);
+    top.postMessage({ highlight: true, id: query.slice(3).trim() }, "*");
+    }                                 
+}
+
 function generateDocument(query: string) {
     if (query && query.slice(0, 3) === "?q=") {
         originPath = PathHelper.getMetadataFilePath(query.slice(3).trim());
@@ -23,19 +46,9 @@ function generateDocument(query: string) {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     if (xhr.responseText) {
-                        const parsedContent = Model.parseJSON(xhr, originPath);
-                        const viewObj = new ViewFactory();
-                        const htmlDOM = viewObj.generate(parsedContent);
-                        if (htmlDOM) {
-                            const url = document.location.search + document.location.hash;
-                            HistoryManager.UpdateHistory(url, parsedContent);
-                            const finale = document.getElementById("divLog");
-                            while (finale.hasChildNodes()) {
-                                finale.removeChild(finale.lastChild);
-                            }
-                            finale.appendChild(htmlDOM);
-                            top.postMessage({ highlight: true, id: query.slice(3).trim() }, "*");
-                        }
+                        parsedContent = Model.parseJSON(xhr, originPath);
+                        sessionStorage.setItem(document.location.search.slice(4), JSON.stringify(parsedContent));
+                        LoadDOM(query);
                     }
                 } else {
                     alert("Invalid json file: " + query);
