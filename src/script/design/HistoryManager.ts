bce4c201464e2ceb1290e09c094d65be6687f07c
Ayush Kumar Baheti <ayush.baheti.kumar@ni.com>
2020-05-12 14:46:45 +0530

    session storage removed navigation to subvis

--- src/script/design/HistoryManager.ts
+++ src/script/design/HistoryManager.ts
@@ -9,9 +9,6 @@ export class HistoryManager {
         top.history.replaceState(stateObj, updatedPath, updatedPath);
         if (document.referrer === "http://127.0.0.1:8081/results.html") {
             sessionStorage.setItem(document.location.search, JSON.stringify(parsedContent));
-        } else {
-        sessionStorage.setItem(document.referrer.substring(document.referrer.indexOf("?")),
-                        JSON.stringify(parsedContent));
         }
     }
     private static queryHeader: string = "?q=";
