1377309f3141f472bb975f0439ab1be04b2e9dff
Ayush Kumar Baheti <ayush.baheti.kumar@ni.com>
2020-05-21 15:04:19 +0530

    integrated convertFromCli() with Convert()

--- src/HTMLGenerator/converter.py
+++ src/HTMLGenerator/converter.py
@@ -28,13 +28,16 @@ def convert(top_level_output_directory, top_level_input_directory, json_with_fil
     # Writing the top, preload, input and output path to the currentConfiguration.txt file.
     try:
         with open(current_configuration_absolute_path, 'w') as current_configuration_file:
-            current_configuration_file.write(
+            if "topPath" in json_with_filepaths:
+                current_configuration_file.write(
                 '/top:' + str(top_level_output_directory / json_with_filepaths["topPath"]) + "\n")
-            current_configuration_file.write(
+            if "preloadedFiles" in json_with_filepaths:
+                current_configuration_file.write(
                 '/preload:' + str(Path(json_with_filepaths["preloadedFiles"])) + "\n")
             current_configuration_file.write(
                 '/input:' + str(top_level_input_directory / json_with_filepaths["inputPath"]) + "\n")
-            current_configuration_file.write(
+            if "outputPath" in json_with_filepaths:    
+                current_configuration_file.write(
                 '/out:' + str(Path(json_with_filepaths["outputPath"])) + "\n")
             current_configuration_file.flush()
 
@@ -65,51 +68,4 @@ def convert(top_level_output_directory, top_level_input_directory, json_with_fil
 def convertFromCli(jsonFilePath) :
     with open(jsonFilePath) as jsonFile:
             json_with_filepaths = json.load(jsonFile)
-            converter_path = Path(Path.cwd() / "ConvertFromConfigurationFile.vi")
-            current_configuration_absolute_path = Path(Path.cwd() / CURRENT_CONFIGURATION_PATH)
-            notification_absolute_path = Path(Path(json_with_filepaths["outputPath"]) / NOTIFICATION_PATH)
-            call_converter_command = str(LABVIEW_PATH) + " " + str(converter_path)
-            try:
-                if os.path.isfile(notification_absolute_path):
-                    os.remove(notification_absolute_path)                
-            except:
-                pass
-            # Writing the top, preload, input and output path to the currentConfiguration.txt file.
-            try:
-                with open(current_configuration_absolute_path, 'w') as current_configuration_file:
-                    if "topPath" in json_with_filepaths:
-                        current_configuration_file.write(
-                        '/top:' + str(json_with_filepaths["topPath"]) + "\n")
-                    current_configuration_file.write(
-                    '/input:' + str(json_with_filepaths["inputPath"]) + "\n")
-                    if "preloadedFiles" in json_with_filepaths:    
-                        current_configuration_file.write(
-                        '/preload:' + str(Path(json_with_filepaths["preloadedFiles"])) + "\n")
-                    if "outputPath" in json_with_filepaths:
-                        current_configuration_file.write(
-                        '/out:' + str(json_with_filepaths["outputPath"]) + "\n")
-                    current_configuration_file.flush()
-
-            except FileNotFoundError as f:
-                error_message = str(f.strerror, _get_path_relative_to_username(f.filename))
-                return error_message
-
-            else:
-                try:
-                    process = subprocess.Popen(call_converter_command, stdin=subprocess.PIPE)
-                except OSError as e:
-                    error_message = str(e)
-                    print(error_message , "\n")
-                    return error_message   
-
-            while(True):
-                if(os.path.isfile(notification_absolute_path)): 
-                    process.stdin.close()
-                    process.wait()
-                    break
-                else:
-                    time.sleep(3)
-            os.remove(notification_absolute_path)
-            os.remove(current_configuration_absolute_path)
-
-            return
+            convert(Path(""),Path(""),json_with_filepaths,Path.cwd())
