087b95561155fa4dbb5b19285a8cc6c6a886fff3
Ayush Kumar Baheti <ayush.baheti.kumar@ni.com>
2020-05-21 11:19:25 +0530

    Added a new function for UI conversion

--- src/HTMLGenerator/converter.py
+++ src/HTMLGenerator/converter.py
@@ -3,6 +3,7 @@ import os.path
 import subprocess
 import pathlib
 from pathlib import Path
+import json
 
 CURRENT_CONFIGURATION_PATH = "currentConfiguration.txt"
 NOTIFICATION_PATH = "complete.txt"
@@ -60,3 +61,54 @@ def convert(top_level_output_directory, top_level_input_directory, json_with_fil
     os.remove(current_configuration_absolute_path)
 
     return
+
+def convertFromCli(jsonFilePath) :
+    with open(jsonFilePath) as jsonFile:
+            json_with_filepaths = json.load(jsonFile)
+            converter_path = Path(Path("C:/FINALE/src/HTMLGenerator") / "ConvertFromConfigurationFile.vi")
+            current_configuration_absolute_path = Path(Path(Path("C:/FINALE/src/HTMLGenerator")) / CURRENT_CONFIGURATION_PATH)
+            notification_absolute_path = Path(Path(json_with_filepaths["outputPath"]) / NOTIFICATION_PATH)
+            call_converter_command = str(LABVIEW_PATH) + " " + str(converter_path)
+            try:
+                if os.path.isfile(notification_absolute_path):
+                    os.remove(notification_absolute_path)                
+            except:
+                pass
+            # Writing the top, preload, input and output path to the currentConfiguration.txt file.
+            try:
+                with open(current_configuration_absolute_path, 'w') as current_configuration_file:
+                    if "topPath" in json_with_filepaths:
+                        current_configuration_file.write(
+                        '/top:' + str(json_with_filepaths["topPath"]) + "\n")
+                    current_configuration_file.write(
+                    '/input:' + str(json_with_filepaths["inputPath"]) + "\n")
+                    if "outputPath" in json_with_filepaths:
+                        current_configuration_file.write(
+                        '/out:' + str(json_with_filepaths["outputPath"]) + "\n")
+                    current_configuration_file.flush()
+
+            except FileNotFoundError as f:
+                error_message = str(f.strerror, _get_path_relative_to_username(f.filename))
+                return error_message
+
+            else:
+                try:
+                    process = subprocess.Popen(call_converter_command, stdin=subprocess.PIPE)
+                except OSError as e:
+                    error_message = str(e)
+                    print(error_message , "\n")
+                    return error_message   
+
+            while(True):
+                if(os.path.isfile(notification_absolute_path)): 
+                    process.stdin.close()
+                    process.wait()
+                    break
+                else:
+                    time.sleep(3)
+            os.remove(notification_absolute_path)
+            os.remove(current_configuration_absolute_path)
+
+            return
+
+convertFromCli("C:\\temp\\file.json")
